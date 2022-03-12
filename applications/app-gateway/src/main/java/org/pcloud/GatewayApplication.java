89258dbe600637279c7fa9c48592f444f746403d
PCloud63514 <pcloud63514@gmail.com>
2022-03-12 23:20:03 +0900

    레디스 추가 문제 발생 Unable to load io.netty.resolver.dns.macos.MacOSDnsServerAddressStreamProvider, fallback to system defaults. This may result in incorrect DNS resolutions on MacOS. 문제 발생 mac os에서 만 발생하는 문제
    intel
    implementation 'io.netty:netty-resolver-dns-native-macos:4.1.68.Final' 추가
    m1
    implementation 'io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-aarch_64' 추가
    단 m1의 의존성의 경우 spring boot version이 2.6.3 이상에서만 찾을 수 있다.
    
    spring boot version 2.5.3 -> 2.6.3으로 변경

--- applications/app-gateway/src/main/java/org/pcloud/GatewayApplication.java
+++ applications/app-gateway/src/main/java/org/pcloud/GatewayApplication.java
@@ -2,9 +2,7 @@ package org.pcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
--- components/module-security/src/main/java/org/pcloud/security/api/AuthApi.java
+++ components/module-security/src/main/java/org/pcloud/security/api/AuthApi.java
@@ -7,6 +7,7 @@ import org.pcloud.security.service.AuthService;
 import org.pcloud.support.token.jwt.JwtToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @RequestMapping("auth")
@@ -16,9 +17,10 @@ public class AuthApi {
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping
-    public JwtTokenResponse issueToken(@RequestBody TokenIssueRequest request) {
+    public Flux<JwtTokenResponse> issueToken(@RequestBody TokenIssueRequest request) {
         JwtToken jwtToken = this.authService.generateToken(request);
-        return new JwtTokenResponse(jwtToken.getToken(), jwtToken.getRefresh());
+        JwtTokenResponse jwtTokenResponse = new JwtTokenResponse(jwtToken.getToken(), jwtToken.getRefresh());
+        return Flux.just(jwtTokenResponse);
     }
 
     @DeleteMapping("{token}")
